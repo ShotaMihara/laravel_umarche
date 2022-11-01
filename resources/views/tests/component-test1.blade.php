71d7315625315f6304bbd3427a3be6d36502826a
ShotaMihara <kreva.biz8354@gmail.com>
2022-11-01 23:32:38 +0900

    変数

--- resources/views/tests/component-test1.blade.php
+++ resources/views/tests/component-test1.blade.php
@@ -2,6 +2,8 @@
     <x-slot name="header">ヘッダー１</x-slot>
     コンポーネント１
 
-    <x-tests.card title="タイトル" content="本文" />
-    
+    <x-tests.card title="タイトル" content="本文" :message="$message"/>
+
+
+
 </x-tests.app> 
