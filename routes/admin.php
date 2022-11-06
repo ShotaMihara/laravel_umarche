29e9c9783a2c4f073c8d5c6eac8b8f0d58ec8590
ShotaMihara <kreva.biz8354@gmail.com>
2022-11-06 22:54:39 +0900

    リソースコントローラー

--- routes/admin.php
+++ routes/admin.php
@@ -9,7 +9,9 @@ use App\Http\Controllers\Admin\Auth\NewPasswordController;
 use App\Http\Controllers\Admin\Auth\PasswordResetLinkController;
 use App\Http\Controllers\Admin\Auth\RegisteredUserController;
 use App\Http\Controllers\Admin\Auth\VerifyEmailController;
+use App\Http\Controllers\Admin\OwnersController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\Auth\OwnersController\Controller;
 
 
 /*
@@ -26,6 +28,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('admin.welcome');
 });
+
+Route::resource('owners', OwnersController::class)
+    ->middleware('auth:admin');
 
 Route::get('/dashboard', function () {
     return view('admin.dashboard');
