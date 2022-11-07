ea959507f951a6311e13cc3df6a0a898b6cd0e6b
ShotaMihara <kreva.biz8354@gmail.com>
2022-11-08 01:22:09 +0900

    Store

--- resources/views/admin/owners/index.blade.php
+++ resources/views/admin/owners/index.blade.php
@@ -10,14 +10,9 @@
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
 
-                        
-                        
-                        
-                    
-
-
 <section class="text-gray-600 body-font">
   <div class="container px-5 mx-auto">
+    <x-flash-message status="info" />
     <div class="lg:w-2/3 w-full mx-auto overflow-auto">
       <table class="table-auto w-full text-left whitespace-no-wrap">
         <thead>
