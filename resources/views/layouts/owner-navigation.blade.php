0378972bef1276ec5d03ba61bdf169bd85da85ad
ShotaMihara <kreva.biz8354@gmail.com>
2022-11-13 01:13:38 +0900

    middleware,abort(404)

--- resources/views/layouts/owner-navigation.blade.php
+++ resources/views/layouts/owner-navigation.blade.php
@@ -3,17 +3,22 @@
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div class="flex justify-between h-16">
             <div class="flex">
-                <!-- Logo -->
+                <!-- Logo -->         
                 <div class="shrink-0 flex items-center">
-                    <a href="{{ route('owner.dashboard') }}">
-                        <x-application-logo class="block h-10 w-auto fill-current text-gray-600" />
-                    </a>
+                    <div class="w-12">
+                        <a href="{{ route('owner.dashboard') }}">
+                            <x-application-logo class="block h-10 w-auto fill-current text-gray-600" />
+                        </a>
+                    </div>
                 </div>
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('owner.dashboard')">
+                    <x-nav-link :href="route('owner.dashboard')" :active="request()->routeIs('owner.dashboard')">
                         {{ __('Dashboard') }}
+                    </x-nav-link>
+                    <x-nav-link :href="route('owner.shops.index')" :active="request()->routeIs('owner.shops.index')">
+                        店舗情報
                     </x-nav-link>
                 </div>
             </div>
@@ -63,8 +68,8 @@
     <!-- Responsive Navigation Menu -->
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
-            <x-responsive-nav-link :href="route('owner.dashboard')" :active="request()->routeIs('owner.dashboard')">
-                {{ __('Dashboard') }}
+            <x-responsive-nav-link :href="route('owner.shops.index')" :active="request()->routeIs('owner.shops.index')">
+                店舗情報
             </x-responsive-nav-link>
         </div>
 
