03895e9c0d1a136dac8686140ecfd11897d82f05
ShotaMihara <kreva.biz8354@gmail.com>
2022-11-21 00:45:06 +0900

    category

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -25,6 +25,7 @@ class DatabaseSeeder extends Seeder
                 ownerSeeder::class,
                 ShopSeeder::class,
                 ImageSeeder::class,
+                CategorySeeder::class,
             ]);
 
     }
